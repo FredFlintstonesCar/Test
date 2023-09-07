cf21164b77b0be0ce19530e20804b7c58629225b
FredFlintstonesCar <144292050+FredFlintstonesCar@users.noreply.github.com>
2023-09-08 00:29:59 +0100

    Update linvm3.ps1

--- linvm3.ps1
+++ linvm3.ps1
@@ -1,14 +1,15 @@
-Remove-AzResourceGroup -Name "RG3" -Force -AsJob
+Remove-AzResourceGroup -Name "RG4" -Force -AsJob
 
 # Variables
-$ResourceGroupName = "RG4"
+$ResourceGroupName = "RG5"
 $location = "East US"  # Change to your desired location
 $vmName = "LinuxVM"
 $adminUsername = "azureuser"  # Change this to your desired username
 $adminPassword = "P@ssw0rd123!"  # Change this to your desired password
 
 # Create a resource group
-az group create --name $ResourceGroupName --location $location
+az group create --name $ResourceGroupName --location $location --quiet -ErrorAction Stop
+echo "RG " & $ResourceGroupName & " created"
 
 # Create a Linux virtual machine
 az vm create `
@@ -18,28 +19,30 @@ az vm create `
     --public-ip-sku Standard `
     --admin-username $adminUsername `
     --admin-password $adminPassword `
-    --size Standard_B2s
+    --size Standard_B2s `
+    --quiet -ErrorAction Stop
 
 # Wait for VM provisioning to complete
 az vm wait --name $vmName --resource-group $ResourceGroupName --created
 
 # Open ports for Nmap and xRDP
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1001
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 22 --priority 1002
+az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1001 --quiet -ErrorAction Stop
+az vm open-port --resource-group $resourceGroupName --name $vmName --port 22 --priority 1002 --quiet -ErrorAction Stop
 
 # Get the public IP address of the VM
 $publicIpAddress = az vm show --resource-group $resourceGroupName --name $vmName --show-details --query "publicIps" --output tsv
-echo "Public IP Address:" 
-echo $publicIpAddress
+echo "Public IP Address: " $publicIpAddress
 
 # Generate SSH key pair
-ssh-keygen -t rsa -b 2048 -f ~/.ssh/azure_ssh_key -N "" -q
+ssh-keygen -t rsa -b 2048 -f ~/.ssh/azure_ssh_key -N "" -q --quiet -ErrorAction Stop
+echo "Key Generated"
 
 # Upload SSH public key to the VM
-az vm user update --resource-group $ResourceGroupName --name $vmName --username $adminUsername --ssh-key-value "$(cat ~/.ssh/azure_ssh_key.pub)"
+az vm user update --resource-group $ResourceGroupName --name $vmName --username $adminUsername --ssh-key-value "$(cat ~/.ssh/azure_ssh_key.pub)" --quiet -ErrorAction Stop
+echo "Key Uploaded"
 
 # Display SSH private key
-cat ~/.ssh/azure_ssh_key
+# cat ~/.ssh/azure_ssh_key
 
 # Install Nmap
 az vm run-command invoke --resource-group $ResourceGroupName --name $vmName --command-id RunShellScript --script "apt-get update && apt-get install -y nmap"
