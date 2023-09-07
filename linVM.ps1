d342154f804cf58171bcafd3625813f92da022aa
FredFlintstonesCar <144292050+FredFlintstonesCar@users.noreply.github.com>
2023-09-07 03:07:48 +0100

    Update linVM.ps1

--- linVM.ps1
+++ linVM.ps1
@@ -1,23 +1,27 @@
 # Variables
-$resourceGroupName = "myres4"
+$ResourceGroupName = "myres5"
 $location = "East US"  # You can change the location as per your preference
 $vmName = "mylinuxvm"
 $adminUsername = "azureuser"  # Change this to your desired username
 $adminPassword = "P@ssw0rd123!"  # Change this to your desired password
 
 # Create a resource group
-az group create --name $resourceGroupName --location $location
+az group create --name $ResourceGroupName --location $location
 
 # Create a Linux virtual machine
 az vm create `
-    --resource-group $resourceGroupName `
+    --resource-group $ResourceGroupName `
     --name $vmName `
     --image "Ubuntu2204" `
     --admin-username $adminUsername `
     --admin-password $adminPassword `
     --size Standard_B2s  # You can change the VM size as needed
 
-$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+# Wait for a few seconds to ensure remoting is enabled
+Start-Sleep -Seconds 30
+
+# Get the Azure VM
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $vmName
 
 # Convert the admin password to a SecureString
 $adminSecurePassword = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force
@@ -26,10 +30,16 @@ $adminSecurePassword = ConvertTo-SecureString -String $adminPassword -AsPlainTex
 $adminCredential = New-Object System.Management.Automation.PSCredential ($adminUsername, $adminSecurePassword)
 
 # Enable PowerShell remoting on the VM
-Enable-AzVMPSRemoting -ResourceGroupName $resourceGroupName -Name $vmName
+Enable-AzVMPSRemoting -ResourceGroupName $ResourceGroupName -Name $vmName
+
+# Wait for a few seconds to ensure remoting is enabled
+Start-Sleep -Seconds 30
 
 # Establish a remote session
-$session = New-PSSession -HostName $vm.Name -Credential $adminCredential
+$session = New-PSSession -ComputerName $vm.Name -Credential $adminCredential
+
+# Wait for a few seconds to ensure remoting is enabled
+Start-Sleep -Seconds 30
 
 # Install nmap and xrdp
 Invoke-Command -Session $session -ScriptBlock {
