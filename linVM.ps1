70d45893bf0814cd25567ed37d14c578518f8127
FredFlintstonesCar <144292050+FredFlintstonesCar@users.noreply.github.com>
2023-09-07 02:34:36 +0100

    Update linVM.ps1

--- linVM.ps1
+++ linVM.ps1
@@ -16,3 +16,23 @@ az vm create `
     --admin-username $adminUsername `
     --admin-password $adminPassword `
     --size Standard_B2s  # You can change the VM size as needed
+
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+# Enable PowerShell remoting on the VM
+Enable-AzVMPSRemoting -ResourceGroupName $resourceGroupName -Name $vmName -Force
+
+# Establish a remote session
+$session = New-PSSession -ComputerName $vm.Name -Credential $vm.Credentials
+
+# Install nmap and xrdp
+Invoke-Command -Session $session -ScriptBlock {
+    sudo apt update
+    sudo apt install nmap xrdp -y
+    sudo systemctl enable xrdp
+    echo xfce4-session >~/.xsession
+    sudo service xrdp restart
+}
+
+# Close the remote session
+Remove-PSSession $session
