87f386c50b7360a4e68867f27d51cb177352d2ce
FredFlintstonesCar <144292050+FredFlintstonesCar@users.noreply.github.com>
2023-09-07 23:45:12 +0100

    Update linvm3.ps1

--- linvm3.ps1
+++ linvm3.ps1
@@ -1,7 +1,7 @@
-Remove-AzResourceGroup -Name "RG6" -Force -AsJob
+Remove-AzResourceGroup -Name "RG7" -Force -AsJob
 
 # Variables
-$ResourceGroupName = "RG7"
+$ResourceGroupName = "RG8"
 $location = "East US"  # Change to your desired location
 $vmName = "LinuxVM"
 $adminUsername = "azureuser"  # Change this to your desired username
@@ -18,18 +18,14 @@ az vm create `
     --public-ip-sku Standard `
     --admin-username $adminUsername `
     --admin-password $adminPassword `
-    --size Standard_B2s `
-    --public-ip-address ""
+    --size Standard_B2s
 
 # Open ports for Nmap and xRDP
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1001 --protocol Tcp
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1001 --protocol Udp
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1002 --protocol Tcp
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1002 --protocol Udp
-az vm open-port --resource-group $resourceGroupName --name $vmName --port 22 --priority 1003 --protocol Tcp
+az vm open-port --resource-group $resourceGroupName --name $vmName --port 3389 --priority 1001 --no-wait
+az vm open-port --resource-group $resourceGroupName --name $vmName --port 22 --priority 1002 --no-wait
 
 # Get the public IP address and store it in a variable
-publicIpAddress=$(az vm show -g $resourceGroupName -n $vmName --query "publicIps" -o tsv)
+$publicIpAddress = az vm show --resource-group $resourceGroupName --name $vmName --query "publicIps" --output tsv
 echo "Public IP Address: $publicIpAddress"
 
 # Generate SSH key pair
@@ -62,6 +58,7 @@ az vm extension set `
     --version 2.1 `
     --settings '{"script": "sudo apt install -y xrdp"}'
 
+echo "running commands"
 # SSH into the VM and install software (example: installing Apache web server)
 az vm extension set --resource-group $resourceGroupName --vm-name $vmName --name customScript --publisher Microsoft.Azure.Extensions --settings '{"script":"#!/bin/bash\nsudo systemctl enable xrdp"}'
 az vm extension set --resource-group $resourceGroupName --vm-name $vmName --name customScript --publisher Microsoft.Azure.Extensions --settings '{"script":"#!/bin/bash\necho xfce4-session >~/.xsession"}'
